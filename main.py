79a90d25947aa4a49f6c6913840e7f0d83562216
iNoma <41831807+ItisNoMatter@users.noreply.github.com>
2025-07-10 04:57:33 +0000

    ファイルを受け取るエントリポイントを作成

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] ="./uploads"
+
 
 @app.route('/')
 def index():
     return 'Hello World!'
+
+
+@app.route("/upload",methods=["POST"])
+def upload():
+    return "aaa"
 
 if __name__ == '__main__':
     app.debug = True
